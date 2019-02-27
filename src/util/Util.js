db4e0ddc7303dda67dacbb9e0636d6300c22e45f
Miroslav L <lemmas.online@gmail.com>
2019-02-27 12:02:54 +0200

    Return result after all promises

--- src/util/Util.js
+++ src/util/Util.js
@@ -2,27 +2,35 @@ const fs = require('fs-extra')
 const axios = require('axios')
 
 exports.sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
-exports.download = async (url, file) => {
-  return fs.ensureFile(file)
-    .then(() => {
-      return axios({
-        url,
-        method: 'GET',
-        responseType: 'stream'
-      })
-        .then(response => {
-          const writer = fs.createWriteStream(file)
-          response.data.pipe(writer)
+exports.download = (url, file) => {
+  const options = {
+    url,
+    method: 'GET',
+    responseType: 'stream'
+  }
 
-          return new Promise((resolve, reject) => {
-            writer.on('finish', resolve)
-            writer.on('error', reject)
-          })
-        })
+  const promises = [
+    fs.ensureFile(file),
+    axios(options)
+  ]
+
+  return Promise.all(promises.map(promise => promise.catch(e => e)))
+    .then(response => {
+      const writer = fs.createWriteStream(file)
+      response.data.pipe(writer)
+
+      return new Promise((resolve, reject) => {
+        writer.on('finish', resolve)
+        writer.on('error', reject)
+      })
     })
 }
 
-exports.writeToFile = async (file, data) => {
-  return fs.ensureFile(file)
-    .then(() => fs.appendFile(file, data))
+exports.writeToFile = (file, data) => {
+  const promises = [
+    fs.ensureFile(file),
+    fs.appendFile(file, data)
+  ]
+
+  return Promise.all(promises.map(promise => promise.catch(e => e)))
 }
